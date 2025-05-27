223dd9397964000c13d9a207179548415a6e912d
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-27 15:48:51 +0200

    added. milestone 2 and test it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import {debounce} from "lodash"
 /*
-📌 Milestone 1: Creare un campo di ricerca e mostrare la lista dei suggerimenti
-Crea un campo di input (<input type="text">) in cui l’utente può digitare.
+📌 Milestone 2: Implementare il Debounce per Ottimizzare la Ricerca
+Attualmente, ogni pressione di tasto esegue una richiesta API. Questo è inefficiente!
+Implementa una funzione di debounce per ritardare la chiamata API fino a quando l’utente smette di digitare per un breve periodo (es. 300ms)
+Dopo l’implementazione, verifica che la ricerca non venga eseguita immediatamente a ogni tasto premuto, ma solo dopo una breve pausa.
 
-Effettua una chiamata API a: 
-/products?search=[query]
-
-La query deve essere sostituita con il testo digitato.
-Mostra i risultati API sotto l'input in una tendina di suggerimenti.
-
-Se l'utente cancella il testo, la tendina scompare.
-
-
-Obiettivo: Mostrare suggerimenti dinamici in base alla ricerca dell'utente.
+Obiettivo: Ridurre il numero di richieste API e migliorare le prestazioni.
 */
 function App() {
 
@@ -20,47 +14,58 @@ function App() {
   const [products, setProducts] = useState([]);
   console.log(products)
 
-  useEffect(() => {
-   if(query.trim() === ""){
-    setProducts([]);
-    return;
-   }
-    //non posso utilizzare l'async all'interno dello useEffect così utilizzo una IIFE
-    (async () => {
-      try{
-        const response = await fetch (`http://localhost:3333/products?search=${query}`)
-        const prodotti = await response.json()
-        setProducts(prodotti);
-      }catch(error){
-        console.error(error)
-      }
+  
 
-    })()
-   
+
+
+  const fetchProducts = async (query) => {
+    if (query.trim() === "") {
+      setProducts([]);
+      return;
+    }
+    try {
+      const response = await fetch(`http://localhost:3333/products?search=${query}`)
+      const prodotti = await response.json()
+      setProducts(prodotti);
+      console.log(query);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const debouncedProducts = useCallback(
+    debounce( fetchProducts, 500)
+    ,[])
+
+
+
+  useEffect(() => {
+    debouncedProducts(query)
   }, [query])
+
 
   return (
     <>
-    <input 
-    className='input'
-    type='text'
-    placeholder='digita il nome del prodotto'
-    value={query}
-    onChange={(e) => setQuery(e.target.value)}
-    />
+      <input
+        className='input'
+        type='text'
+        placeholder='digita il nome del prodotto'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
 
-    {
-      products.length > 0 &&(
-        <div className='container-products'>
+      {
+        products.length > 0 && (
+          <div className='container-products'>
             {
               products.map((p) => (
                 <p key={p.id}>{p.name}</p>
               ))
             }
-        </div>
-      )
-    }
-      
+          </div>
+        )
+      }
+
     </>
   )
 }
